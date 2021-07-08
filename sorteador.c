0ac9dcab4ca92c312fc51af68b1b4fcc45c481f8
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2021-07-07 21:28:09 -0300

    Create sorteador.c

---
+++ sorteador.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int qtdnomes;
+	int i;
+	int num_aleat;
+	
+	printf("Digite a quantidade de nomes a serem sorteados:\n");
+	scanf(" %i", &qtdnomes);
+	fflush(stdin);
+	
+	char nome[qtdnomes][15];
+	
+	for(i=0;i<qtdnomes;i++){
+		printf("Digite o primeiro nome:\n");
+		scanf(" %s", &nome[i]);
+	}
+	
+	num_aleat = rand() % qtdnomes;
+	
+	printf("O nome escolhido foi: %s", nome[num_aleat]);
+	
+	return 0;
+}
